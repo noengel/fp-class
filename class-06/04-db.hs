264c4a54ff8d09e38821f155ceef0fa0b79644a3
Tselin Eugene <cevge@yandex.ru>
2014-10-20 16:24:14 +0400

    up to 5.0

--- class-06/04-db.hs
+++ class-06/04-db.hs
@@ -13,4 +13,53 @@
 
 -}
 
-main = undefined
+import System.Environment
+import System.IO
+import Data.List
+
+splitString :: String -> [String]
+splitString str = filter (/= ";") $ groupBy (\x y -> x /= ';' && y /= ';') str
+
+-- Список студентов заданной группы
+--showOneGroup :: Int -> Int -> [[String]] -> [[String]]
+--showOneGroup course group students = filter (\[_,_,c,g] -> (read c == course) && (read g == group)) students  
+
+-- 1) Вычислить средний возраст студентов заданной группы заданного курса
+f1 :: FilePath -> Int -> Int -> IO ()
+f1 fname course group = do
+	file <- readFile fname
+	let 
+		students = showOneGroup course group (map splitString (lines file))
+	let 
+		(avg, count) = foldl (\(av, c) [_,a,_,_] -> (av + read a, c + 1)) (0,0) students
+	putStrLn $ show (avg / count)
+	where
+		showOneGroup course group students = filter (\[_,_,c,g] -> (read c == course) && (read g == group)) students  
+
+-- Сортировка по курсу и группе
+sortByGroups :: (Int, Int) -> (Int, Int) -> Ordering
+sortByGroups (x,y) (z,t)
+	| x < z = LT
+	| x > z = GT
+	| y < t = LT
+	| y > t = GT
+	| otherwise = EQ
+
+-- 2) Вычислить количество студентов в каждой группе каждого курса
+f2 :: FilePath -> IO ()
+f2 fname = do
+	file <- readFile fname
+	let 
+		list = group (sortBy sortByGroups (map (\[_,_,c,g] -> (read c, read g)) (map splitString (lines file))))
+	let 
+		count = map (\xs -> (head xs, length xs)) list
+	putStrLn $ unlines (map (\(x,y) -> show x ++ ": " ++ show y) count)
+
+case' :: Int -> [String] -> IO ()
+case' 1 (x1:x2:x3:_) = f1 x1 (read x2) (read x3)
+case' 2 (x1:_) = f2 x1
+case' 3 (x1:_) = undefined
+
+main = do
+	(n:args) <- getArgs
+	case' (read n) args
