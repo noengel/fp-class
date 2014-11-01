7515d02ba8070710c59e0f6eea3b1da7e318cea1
Tselin Eugene <cevge@yandex.ru>
2014-11-02 00:11:28 +0300

    up to 03

--- class-08/01-expressions.hs
+++ class-08/01-expressions.hs
@@ -20,7 +20,7 @@ eval (e1 `Mul` e2) = eval e1 * eval e2
 -}
 
 instance Eq Expr where
-  (==) = undefined
+	(==) a b = (eval a) == (eval b)
 
 {-
   Реализуйте для этого типа экземпляр класса типов Show так,
@@ -30,7 +30,11 @@ instance Eq Expr where
 -}
 
 instance Show Expr where
-  show = undefined
+	show (I a) = show a
+	show (a `Add` b) = show a ++ "+" ++ show b
+	show ((a `Add` b) `Mul` c) = "(" ++ show (a `Add` b) ++ ")*" ++ show c
+	show (a `Mul` (b `Add` c)) = show a ++ "*(" ++ show (b `Add` c) ++ ")"
+	show (a `Mul` b) = show a ++ "*" ++ show b
 
 -- Тесты
 test = all (== expr 4) exprs
@@ -47,3 +51,8 @@ test = all (== expr 4) exprs
 {-
   Напишите экземпляр класса типов Ord, который сравнивает выражения по их значению.
 -}
+
+instance Ord Expr where
+	compare a b = compare (eval a) (eval b)
+	
+test2 = ((I 5 `Add` I 1) `Mul` I 7) >= (I 6 `Add` (I 6`Mul` I 6))
--- class-08/02-listable.hs
+++ class-08/02-listable.hs
@@ -4,9 +4,27 @@
    fromList :: [a] -> a
 -}
 
-
+class Listable a where
+    toList :: a -> [a]
+    fromList :: [a] -> a
+    
 {-
   Объявите экземпляры класса типов Listable для следующих типов:
   1) String - строка разбивается по пробелам на список слов.
   2) Integral a - любое целое число разбивается на список цифр.
 -}
+
+instance Listable String where
+    toList = words
+    fromList = unwords
+
+
+instance Listable Integer where
+    toList i = reverse $ toDigits i
+    fromList i = fromDigits i
+
+toDigits i
+    | i == 0 = []
+    | otherwise = (i `mod` 10) : (toDigits (i `div` 10))
+
+fromDigits = foldl (\acc x -> acc * 10 + x) 0
