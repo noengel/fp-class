57b803d50afe9f484900287f727b25ebe714c0e8
Tselin Eugene <cevge@yandex.ru>
2014-11-23 17:17:44 +0300

    up to 1

--- class-11/ex1.hs
+++ class-11/ex1.hs
@@ -19,30 +19,6 @@ data Operation = Summ | Mult | Div
 data Config = Config Operation Int
     deriving (Show)
     
-summand :: Integer -> Reader Integer Integer
-summand x = do
-    n <- ask
-    return (x + n)
-
-multiplier :: Integer -> Reader Integer Integer
-multiplier x = do
-    n <- ask
-    return (x * n)
-    
-division :: Integer -> Reader Integer Integer
-division x = do
-    n <- ask
-    return (x `div` n)
-    
---subwork :: Reader Config ()
-subwork = undefined    
-
---work :: Reader Config ()
-work = undefined
-    
-load_file :: FilePath -> IO String
-load_file file = readFile file
-    
 load_config :: String -> [Config]
 load_config = map (\str -> read_config str) . lines
 
@@ -50,7 +26,22 @@ read_config :: String -> Config
 read_config str
     | oper == "summand" = Config Summ $ read $snd $ span ( == '=') oth
     | oper == "multiplier" = Config Mult $ read $ snd $ span ( == '=') oth
-    | oper == "division" = Config Div $ read $ snd $ span ( == '=') oth
+    | oper == "divisor" = Config Div $ read $ snd $ span ( == '=') oth
         where (oper, oth) = span (/= '=') str
-        
-main = undefined
+
+work :: Int -> Reader [Config] Int
+work num = do
+  cfg <- ask
+  return $ foldl action num cfg
+    
+action :: Int -> Config -> Int
+action a (Config Summ b) = a + b
+action a (Config Mult b) = a * b
+action a (Config Div b) = a `div` b
+  
+main = do
+    [cfg_file, num_file] <- getArgs
+    nums <- readFile num_file
+    cfg_data <- readFile cfg_file
+    let cfg = load_config cfg_data
+    mapM_ (\num -> (print . runReader (work num)) cfg) $ map read $ lines nums
