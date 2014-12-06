84764fc162674f9d64c3035be0ad91386bea3056
Tselin Eugene <cevge@yandex.ru>
2014-12-06 09:29:57 +0300

    up to 5.0

--- class-13/02-float_complex.hs
+++ class-13/02-float_complex.hs
@@ -1,10 +1,27 @@
 import Parser
 import SimpleParsers
 import ParseNumbers
+import Control.Applicative hiding (many)
+import Data.Char
 
 {- Напишите парсер для вещественных чисел. -}
 float :: Parser Float
-float = undefined
+float = do (*) <$> minus <*>(positiveFloat <|> fromIntegral <$> natural)
+    where
+        positiveFloat = do
+            a <- natural
+            char '.'
+            b <- fraction
+            if a >= 0 then 
+                return $ fromIntegral a + b
+            else 
+                return $ fromIntegral a - b
+                
+        minus = (char '-' >> return (-1)) <|> return 1
+        
+fraction = do
+    num <- many digit
+    return $ foldr (\ d -> (/ fromIntegral 10) . (+ fromIntegral d)) 0 num
 
 {-
   Напишите парсер для представления комплексных чисел,
@@ -13,14 +30,18 @@ float = undefined
   
 -}
 complex :: Parser (Float, Float)
-complex = undefined
+complex = bracket "(" ")" $ do
+    a <- token float
+    char ','
+    b <- token float
+    return $ (a, b)
 
 {-
   Напишите парсер для списка комплексных чисел (разделитель — точка с запятой),
   заключённого в квадратные скобки.
 -}
 complexList :: Parser [(Float, Float)]
-complexList = undefined
+complexList = bracket "[" "]" $ sepBy (token complex) (symbol ";")
 
 {-
   Модифицируйте предыдущий парсер таким образом, чтобы в исходной строке
@@ -28,7 +49,11 @@ complexList = undefined
   при этом должна считаться равной нулю).
 -}
 complexList2 :: Parser [(Float, Float)]
-complexList2 = undefined
+complexList2 = bracket "[" "]" $ sepBy (token complex<|>float_to_complex) (symbol ";")
+
+float_to_complex = do
+    fl <- token float
+    return (fl,0)
 
 {-
    Модифицируйте предыдущий парсер таким образом, чтобы компоненты списка
@@ -36,6 +61,6 @@ complexList2 = undefined
    требуемое с помощью вспомогательных парсеров, допускающих повторное применение.
 -}
 complexList3 :: Parser [(Float, Float)]
-complexList3 = undefined
+complexList3 = bracket "[" "]" $ sepBy (token complex<|>float_to_complex) (symbol ",")
 
 
